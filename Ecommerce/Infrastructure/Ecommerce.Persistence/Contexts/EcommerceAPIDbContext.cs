fe054cb6c1b6e86f09c84c323749a4b7770136db
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-10-31 15:20:25 +0400

    Db Context Add

---
+++ Ecommerce/Infrastructure/Ecommerce.Persistence/Contexts/EcommerceAPIDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Persistence.Contexts;
+
+public class EcommerceAPIDbContext : DbContext
+{
+    public EcommerceAPIDbContext(DbContextOptions<EcommerceAPIDbContext> options) : base(options)
+    { }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<Product> Products { get; set; }
+}
