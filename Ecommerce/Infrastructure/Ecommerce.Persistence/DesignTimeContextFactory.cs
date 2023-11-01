a2725af2ff1cc5039aee860a9d135a36561b2cdb
MrAzimzadeh <mehemmedezimzase@gmail.com>
2023-11-01 16:31:33 +0400

    CLI design Time Error

---
+++ Ecommerce/Infrastructure/Ecommerce.Persistence/DesignTimeContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ecommerce.Persistence;
+
+public class DesignTimeContextFactory : IDesignTimeDbContextFactory<EcommerceAPIDbContext>
+{
+    public EcommerceAPIDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<EcommerceAPIDbContext>();
+        optionsBuilder.UseNpgsql
+            ("User ID=docker;Password=docker;Host=localhost;Port=5432;Database=EcommerceAPIDb;");
+        return new EcommerceAPIDbContext(optionsBuilder.Options);
+    }
+}
